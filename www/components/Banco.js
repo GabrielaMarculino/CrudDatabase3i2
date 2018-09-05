f90ade27ba7676305ef52f9e711d6b9027a8cbac
GabrielaMarculino <gabrielamarculino@gmail.com>
2018-09-06 03:38:24 +0900

    Project's Initial Commit

---
+++ www/components/Banco.js
@@ -0,0 +1,19 @@
+$(document).on("click", "#btnEnviar", function(){
+var parametros = {
+  "nome": $("#txtNome").val(),
+  "cpf": $("#txtCpf").val(),
+};
+$.ajax({
+    type:"post",
+    url:"https://crudmobile3i2-gabrielamarculino.c9users.io/cadastro.php", 
+    data:parametros,
+    success:function(data){
+      navigator.notification.alert(data);
+      $("txtNome").val("");
+      $("txtCpf").val("");
+    },
+    error:function(data){
+      navigator.notification.alert("erro: "+data);
+    },
+  });
+});
